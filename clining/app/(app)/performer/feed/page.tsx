f2fc2da3b71c895ba6976254b477f651eef50a5a
Serhei <s.chikita@mail.ru>
2026-03-08 22:37:10 +0300

    edit orders

--- clining/app/(app)/performer/feed/page.tsx
+++ clining/app/(app)/performer/feed/page.tsx
@@ -99,7 +99,7 @@ export default function FeedPage() {
         matchesUrgent
       );
     });
-  }, [filters]);
+  }, [filters, orders]);
 
   return (
     <div className="min-h-screen bg-slate-50 dark:bg-slate-950">
